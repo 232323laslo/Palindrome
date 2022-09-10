d8e9bedfce8c3fbbaf7882a79499102ddaca0005
Vladyslav Hryshchenko <vladyslav_hryshchenko@proton.me>
2022-09-10 14:31:47 +0300

    Update Palindrome.py

--- Palindrome.py
+++ Palindrome.py
@@ -1,4 +1,3 @@
-# объявление функции
 def is_palindrome(text):
     text1="".join(c for c in text if c.isalpha())
     text1 = text1.lower()
@@ -13,4 +12,18 @@ txt = input('Your text: ')
 
 print(is_palindrome(txt))
 
+
+
+# The is_palindrome(text) function, which takes the string text as an argument and returns True if the specified text is a palindrome and False otherwise.
+
+# True: 
 # ΝΙΨΟΝ ΑΝΟΜΗΜΑΤΑ ΜΗ ΜΟΝΑΝ ΟΨΙΝ
+# Madam, I'm Adam.
+# Sit on a potato pan, Otis. 
+# Able was I, ere I saw Elba.
+
+# False:
+# Jesus travels toward Jerusalem 
+# The disciples ask about the future
+# the Christ, the son of the living God
+# When Jesus had finished...
